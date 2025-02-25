3db400f33b11f7addf69c0757c2946cc126c08f1
Katrine <kana020@student.kristiania.no>
2025-02-25 15:01:59 +0100

    katrine

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -1,51 +1,3 @@
-/*import React, { useEffect, useRef } from "react";
-import { Map, View } from "ol";
-import TileLayer from "ol/layer/Tile";
-import { OSM } from "ol/source";
-import { useGeographic } from "ol/proj";
-import GeoJSON from "ol/format/GeoJSON";
-
-import "ol/ol.css";
-import VectorSource from "ol/source/Vector";
-import VectorLayer from "ol/layer/Vector";
-import { Fill, Stroke, Style } from "ol/style";
-
-useGeographic();
-
-//const map = new Map({
-//view: new View({ center: [10.8, 59.9], zoom: 13 }),
-//layers: [new TileLayer({ source: new OSM() })],
-//});
-
-export function Application() {
-  const mapRef = useRef<HTMLDivElement | null>(null);
-  useEffect(() => {
-    const Sivilforsvarsdistrikter = new VectorLayer({
-      source: new VectorSource({
-        url: "/geojson/Sivilforsvarsdistrikter.json",
-        format: new GeoJSON(),
-      }),
-      style: new Style({
-        fill: new Fill({ color: "rgba(0, 0, 255, 0.3)" }),
-        stroke: new Stroke({ color: "#0000FF", width: 2 }),
-      }),
-    });
-
-    const map = new Map({
-      target: mapRef.current!,
-      view: new View({ center: [10.8, 59.9], zoom: 6 }),
-      layers: [
-        new TileLayer({ source: new OSM() }), // Bakgrunnskart
-        Sivilforsvarsdistrikter, // Polygonlag
-      ],
-    });
-
-    map.setTarget(mapRef.current!);
-  }, []);
-
-  return <div ref={mapRef}></div>;
-}*/
-
 import React, { useEffect, useRef } from 'react'
 import { Map, View } from 'ol'
 import TileLayer from 'ol/layer/Tile'
@@ -67,7 +19,7 @@ export function Application() {
     // **LAG 1: Sivile forsvarsregioner (polygons)**
     const SivilforsvarsdistrikterLayer = new VectorLayer({
       source: new VectorSource({
-        url: '/geojson/Sivilforsvarsdistrikter.json',
+        url: '/ArbeidskravBase/geojson/Sivilforsvarsdistrikter.json',
         format: new GeoJSON(),
       }),
       style: new Style({
@@ -79,7 +31,7 @@ export function Application() {
     // **LAG 2: Nødhus (punkter)**
     const OffentligeTilfluktsromLayer = new VectorLayer({
       source: new VectorSource({
-        url: '/geojson/OffentligeTilfluktsrom.json',
+        url: '/ArbeidskravBase/geojson/OffentligeTilfluktsrom.json',
         format: new GeoJSON(),
       }),
       style: new Style({
